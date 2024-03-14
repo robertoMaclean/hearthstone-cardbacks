0a5c77822ebf7c2f660140f6fc54d87bbad661ab
Roberto Mac lean <roberto.macleanmarinquez@externos-cl.cencosud.com>
2024-03-14 09:54:16 -0300

    feat: add validators

--- src/app/cardbacks/cardback-form/cardback-form.component.ts
+++ src/app/cardbacks/cardback-form/cardback-form.component.ts
@@ -1,10 +1,16 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { Cardback } from '../carback';
 import { CardbacksService } from '../cardbacks.service';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-cardback-form',
@@ -14,6 +20,7 @@ import { MatButtonModule } from '@angular/material/button';
     ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
+    NgIf,
   ],
   templateUrl: './cardback-form.component.html',
   styleUrl: './cardback-form.component.scss',
@@ -23,17 +30,35 @@ export class CardbackFormComponent {
   cardback: Cardback | null = null;
   @Output() refreshCardbackEvent = new EventEmitter<void>();
   @Output() cleanNewEvent = new EventEmitter<void>();
+  public urlRegex =
+    /(^|\s)((http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))?)/gi;
   cardbackForm = new FormGroup({
-    name: new FormControl(''),
-    description: new FormControl(''),
-    howToGet: new FormControl(''),
-    img: new FormControl(''),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
+    description: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(220),
+    ]),
+    howToGet: new FormControl('', [
+      Validators.minLength(10),
+      Validators.maxLength(130),
+    ]),
+    img: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(200),
+      Validators.pattern(this.urlRegex),
+    ]),
   });
 
   constructor(private cardbackService: CardbacksService) {}
 
   ngOnInit() {
     if (this.cardback) {
+      console.log('in set cardback', this.cardback);
       this.setCardbackForm(this.cardback);
     }
   }
