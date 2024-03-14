f0a9bcdaec76318191bda54555f9f15354838ff2
Roberto Mac lean <roberto.macleanmarinquez@externos-cl.cencosud.com>
2024-03-14 10:14:07 -0300

    feat: add page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,18 @@ import { CardbacksComponent } from './cardbacks/cardbacks.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { NgIf } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardbacksComponent, MatProgressBarModule, NgIf],
+  imports: [
+    RouterOutlet,
+    CardbacksComponent,
+    MatProgressBarModule,
+    NgIf,
+    MatToolbarModule,
+  ],
   providers: [HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
